40b368cae6c90abbabff2c25ad3b9d4f7286c83e
Tomas Gonzalez <tomasg88@gmail.com>
2022-03-08 01:13:10 -0300

    rrss icons

--- web/components/rrss/RRSSIcons.jsx
+++ web/components/rrss/RRSSIcons.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { FaFacebookSquare, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaSpotify, FaYoutube } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
 import Link from "next/link";
 import styles from "./RRSSIcons.module.scss";
 
 export default function RRSSIcons() {
   return (
     <div className={styles.container}>
+      <Link href="https://instagram.com/enciendetupoder">
+        <a target="_blank">
+          <AiFillInstagram color="#a6895a" size={48} />
+        </a>
+      </Link>
       <Link href="https://www.facebook.com/enciendetupoder">
         <a target="_blank">
-          <FaFacebookSquare color="#a6895a" size={40} />
+          <FaSpotify color="#a6895a" size={40} />
+        </a>
+      </Link>
+      <Link href="https://www.facebook.com/enciendetupoder">
+        <a target="_blank">
+          <FaYoutube color="#a6895a" size={40} />
         </a>
       </Link>
       <Link href="https://api.whatsapp.com/send?phone=5491168640045">
         <a target="_blank">
-          <FaWhatsapp color="#a6895a" size={40} />
+          <IoLogoWhatsapp color="#a6895a" size={40} />
         </a>
       </Link>
-      <Link href="https://instagram.com/enciendetupoder">
+      <Link href="https://api.whatsapp.com/send?phone=5491168640045">
         <a target="_blank">
-          <FaInstagram color="#a6895a" size={40} />
+          <MdEmail color="#a6895a" size={44} />
         </a>
       </Link>
     </div>
