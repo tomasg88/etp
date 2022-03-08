c8f5bf028e28ad9f35de6e7cdf726b9a29406e11
Tomas Gonzalez <tomasg88@gmail.com>
2022-03-08 00:56:55 -0300

    next config options

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["media-private.canva.com"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "${path.join(__dirname, "styles")}/_variables.scss";`,
   },
 };
 
