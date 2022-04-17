589393a68513c89ca0f21bcb40b0958a18e4d2d7
Tomas Gonzalez <tomasg88@gmail.com>
2022-04-17 20:22:11 -0300

    update tailwind theme

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          300: "#D0AB67",
+          400: "#C8AC86",
+          600: "#9B8054",
+        },
+        orange: {
+          400: "#F7966B",
+        },
+        pink: {
+          400: "#FCDDD6",
+        },
+        purple: {
+          300: "#B19ACA",
+          400: "#8D64AA",
+        },
+        salmon: {
+          400: "#FCD3C1",
+        },
+        skin: {
+          400: "#FCD3C1",
+        },
+      },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        playfair: ["Playfair"],
+        "proxima-nova": ['"Proxima Nova"'],
+      },
+    },
   },
   plugins: [],
-};
+}
