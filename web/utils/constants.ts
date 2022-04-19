0f64c64795631ebb79d30420fc49a46b3535b58e
Tomas Gonzalez <tomasg88@gmail.com>
2022-04-19 19:53:17 -0300

    constants file

---
+++ web/utils/constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_PAGE_URL = 'https://enciendetupoder.com';
+export const DEFAULT_TITLE = 'https://enciendetupoder.com';
+export const DEFAULT_DESCRIPTION = 'https://enciendetupoder.com';
+export const DEFAULT_IMAGE = 'https://enciendetupoder.com';
