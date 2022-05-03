45b2d58d1d38b44fa3b04fcf70cbdedcfb54e11f
Tomas Gonzalez <tomasg88@gmail.com>
2022-05-03 11:51:32 -0300

    add eslint-plugin, prettier change and lint rule

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  extends: "next/core-web-vitals",
+  extends: 'next/core-web-vitals',
   rules: {
-    "@next/next/no-img-element": "off",
-    "no-unused-vars": "error",
-    "react/jsx-curly-brace-presence": [
-      "error",
-      { props: "never", children: "never" },
+    '@next/next/no-img-element': 'off',
+    'no-unused-vars': 'error',
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'never' },
     ],
-    "react/jsx-sort-props": "error",
-    "react/jsx-no-useless-fragment": "error",
+    'react/jsx-no-useless-fragment': 'error',
+    'react/jsx-sort-props': 'error',
+    'react/jsx-no-useless-fragment': 'error',
   },
 }
